bf16e786f0e36c383bb5a25ebf6258783c22c811
Ahmed-044 <128463984+Ahmed-044@users.noreply.github.com>
2023-06-09 10:54:36 +0500

    Update Dashboard.tsx

--- erp_frontend/components/dashboard/Dashboard.tsx
+++ erp_frontend/components/dashboard/Dashboard.tsx
@@ -2,6 +2,8 @@
  * Template credit: https://material-ui.com/getting-started/templates/
  */
 
+//This React functional component creates a dashboard interface using Material-UI for styling. It dynamically generates a personalized welcome message based on the user's type, enhancing the user experience with customized content.
+
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
